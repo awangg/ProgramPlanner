7489694427542db1462d304ccb6af5467303605e
Andy Wang <andynmnwang@gmail.com>
2020-07-17 14:34:24 -0400

    remove response code

--- api/events/create.php
+++ api/events/create.php
@@ -18,7 +18,6 @@
       "description" => $event_description
     ));
   }else {
-    http_response_code(500);
     echo json_encode(array(
       "error" => "Failed to INSERT into database"
     ));
